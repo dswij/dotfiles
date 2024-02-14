743d548744ab02a9c41d814587110f27df5049a6
dswij <dswijj@gmail.com>
2024-02-14 14:37:24 +0800

    nvim: rework neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- set leader on top before bootstrap due to `lazy`
+vim.g.mapleader = " "
+vim.wo.relativenumber = true
+
+require("bootstrap")
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,89 @@
+--- TODO: split this into own plugins
+return {
+	"nvim-lua/plenary.nvim",
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			vim.cmd([[colorscheme onedark]])
+		end,
+	},
+	"folke/neodev.nvim",
+	"folke/which-key.nvim",
+	{ "nvim-treesitter/nvim-treesitter", init = function() end },
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	-- show leading whitespace
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
+	-- better match
+	{
+		"andymass/vim-matchup",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end,
+	},
+	-- cd to root git
+	{
+		"notjedi/nvim-rooter.lua",
+		config = function()
+			require("nvim-rooter").setup()
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>gf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end,
+	},
+	{
+		"ckipp01/stylua-nvim",
+		config = function()
+			run = "cargo install stylua"
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = { "rust" },
+		config = function()
+			vim.g.rustfmt_autosave = 1
+			vim.g.rustfmt_emit_files = 1
+			vim.g.rustfmt_fail_silently = 0
+		end,
+	},
+	{
+		"junegunn/fzf.vim",
+		dependencies = {
+			{ "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
+		},
+		config = function()
+			vim.g.fzf_layout = { down = "~20%" }
+		end,
+	},
+	{
+		"plasticboy/vim-markdown",
+		ft = { "markdown" },
+		dependencies = {
+			"godlygeek/tabular",
+		},
+		config = function()
+			-- never ever fold!
+			vim.g.vim_markdown_folding_disabled = 1
+			-- support front-matter in .md files
+			vim.g.vim_markdown_frontmatter = 1
+			-- 'o' on a list item should insert at same level
+			vim.g.vim_markdown_new_list_item_indent = 0
+			-- don't add bullets when wrapping:
+			-- https://github.com/preservim/vim-markdown/issues/232
+			vim.g.vim_markdown_auto_insert_bullets = 0
+		end,
+	},
+}
